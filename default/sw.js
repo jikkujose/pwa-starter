59db3b8e98adc290ac0dc77b9c462caa3263fcc2
Jikku Jose <jikkujose@gmail.com>
2021-11-14 14:35:00 +0530

    Separate service worker

--- default/sw.js
+++ default/sw.js
@@ -1,7 +1,7 @@
 self.addEventListener("install", function(event) {
   event.waitUntil(
     caches.open("sw-cache").then(function(cache) {
-      return cache.add("index.html")
+      return cache.addAll(["index.html"])
     })
   )
 })
